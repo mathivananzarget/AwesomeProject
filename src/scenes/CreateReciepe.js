847e9e40373897ff7f30a7b462ac8b43e85d0d6d
mathivananzarget <mathivanan@zarget.com>
2017-05-06 20:12:31 +0530

    timeline

--- src/scenes/CreateReciepe.js
+++ src/scenes/CreateReciepe.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactNative from 'react-native';
+import Timeline from 'react-native-timeline-listview';
 
 const {
   AppRegistry,
@@ -25,43 +26,126 @@ import { Col, Row, Grid } from 'react-native-easy-grid';
 
 export default class CreateReciepe extends Component {
 
-constructor(props){
-   super(props);
-     this.clickMe=this.clickMe.bind(this);
-  }
-    static navigationOptions = {
-title: "Create Your Own Reciepe"
-  };
+constructor(){
+    super()
+    this.onEventPress = this.onEventPress.bind(this)
+    this.renderSelected = this.renderSelected.bind(this)
+    this.renderDetail = this.renderDetail.bind(this)
 
-  clickMe() {
-    const { navigate } = this.props.navigation;
-    navigate('Home',{'profileData':this.props});
+    this.data = [
+      {
+        time: '09:00', 
+        title: 'Archery Training', 
+        description: 'The Beginner Archery and Beginner Crossbow course does not require you to bring any equipment, since everything you need will be provided for the course. ',
+        lineColor:'#009688', 
+        imageUrl: 'https://cloud.githubusercontent.com/assets/21040043/24240340/c0f96b3a-0fe3-11e7-8964-fe66e4d9be7a.jpg'
+      },
+      {
+        time: '10:45', 
+        title: 'Play Badminton', 
+        description: 'Badminton is a racquet sport played using racquets to hit a shuttlecock across a net.', 
+        imageUrl: 'https://cloud.githubusercontent.com/assets/21040043/24240405/0ba41234-0fe4-11e7-919b-c3f88ced349c.jpg'
+      },
+      {
+        time: '12:00', 
+        title: 'Lunch'
+              },
+      {
+        time: '14:00', 
+        title: 'Watch Soccer', 
+        description: 'Team sport played between two teams of eleven players with a spherical ball. ',
+        lineColor:'#009688', 
+        imageUrl: 'https://cloud.githubusercontent.com/assets/21040043/24240419/1f553dee-0fe4-11e7-8638-6025682232b1.jpg'
+      },
+      {
+        time: '16:30', 
+        title: 'Go to Fitness center', 
+        description: 'Look out for the Best Gym & Fitness Centers around me :)', 
+        imageUrl: 'https://cloud.githubusercontent.com/assets/21040043/24240422/20d84f6c-0fe4-11e7-8f1d-9dbc594d0cfa.jpg'
+      }
+    ]
+    this.state = {selected: null}
+  } 
+
+  onEventPress(data){
+    this.setState({selected: data})
   }
-   render() {
+
+  renderSelected(){
+      if(this.state.selected)
+        return <Text style={{marginTop:10}}>Selected event: {this.state.selected.title} at {this.state.selected.time}</Text>
+  }
+
+  renderDetail(rowData, sectionID, rowID) {
+    let title = <Text style={[stylesnew.title]}>{rowData.title}</Text>
+    var desc = null
+    if(rowData.description && rowData.imageUrl)
+      desc = (
+        <View style={stylesnew.descriptionContainer}>   
+          <Image source={{uri: rowData.imageUrl}} style={stylesnew.image}/>
+          <Text style={[stylesnew.textDescription]}>{rowData.description}</Text>
+        </View>
+      )
+    
     return (
-      <Container style={{ backgroundColor: '#373D4B'}}>
-                <Content>
-                <Grid>
-                <Row style={{ backgroundColor: '#FFF8EE' , margin:10,marginTop:0,height:540  }}>
+      <View style={{flex:1}}>
+        {title}
+        {desc}
+      </View>
+    )
+  }
 
-                <Text style={styles.instructions}>
-		          create page
-		        </Text>
-		           <Button
-		          onPress={this.clickMe.bind(this,'play')}
-		          title="Go Back"
-		          color="#841584"
-		          accessibilityLabel="Learn more about this purple button"
-		        />
-		        <Text style={styles.instructions}>
-		          Double tap R on your keyboard to reload,{'\n'}
-		          Shake or press menu button for dev menu
-		        </Text>
-                
-                  </Row>
-                  </Grid>
-                </Content>
+  render() {
+    return (
+           <Container style={{ backgroundColor: '#FFF',margin:8}}>
+        {this.renderSelected()}
+        <Timeline 
+          style={stylesnew.list}
+          data={this.data}
+          circleSize={20}
+          circleColor='rgba(0,0,0,0)'
+          lineColor='rgb(45,156,219)'
+          timeContainerStyle={{minWidth:52, marginTop: -5}}
+          timeStyle={{textAlign: 'center', backgroundColor:'#ff9797', color:'white', padding:5, borderRadius:13}}
+          descriptionStyle={{color:'gray'}}
+          options={{
+            style:{paddingTop:5}
+          }}
+          onEventPress={this.onEventPress}
+          renderDetail={this.renderDetail}
+        />
+
             </Container>
     );
   }
+
 }
+const stylesnew = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+	paddingTop:65,
+    backgroundColor:'white'
+  },
+  list: {
+    flex: 1,
+    marginTop:20,
+  },
+  title:{
+    fontSize:16,
+    fontWeight: 'bold'
+  },
+  descriptionContainer:{
+    flexDirection: 'row',
+    paddingRight: 50
+  },
+  image:{
+    width: 50,
+    height: 50,
+    borderRadius: 25
+  },
+  textDescription: {
+    marginLeft: 10,
+    color: 'gray'
+  }
+});
--- styles.js
+++ styles.js
@@ -65,6 +65,33 @@ var styles = StyleSheet.create({
     fontSize: 22,
     margin:8,
     height: 42
+  },
+  container: {
+    flex: 1,
+    padding: 20,
+  paddingTop:65,
+    backgroundColor:'white'
+  },
+  list: {
+    flex: 1,
+    marginTop:20,
+  },
+  title:{
+    fontSize:16,
+    fontWeight: 'bold'
+  },
+  descriptionContainer:{
+    flexDirection: 'row',
+    paddingRight: 50
+  },
+  image:{
+    width: 50,
+    height: 50,
+    borderRadius: 25
+  },
+  textDescription: {
+    marginLeft: 10,
+    color: 'gray'
   }
 })
 
